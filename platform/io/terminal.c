2ef9c2d3de54e39d695db4ca91e2812ad0c29cb3
Robert <robert.3dgamedev@gmail.com>
2024-08-20 19:58:11 +0200

    Add red bold

--- platform/io/terminal.c
+++ platform/io/terminal.c
@@ -1,6 +1,7 @@
 // Copyright (c) 2024 Robert Hildebrandt. All rights reserved.
 #include "terminal.h"
 
+const char* TERM_RED_BOLD = "";
 const char* TERM_GREEN_BOLD = "";
 const char* TERM_NORMAL = "";
 
@@ -16,17 +17,21 @@ void term_init()
   {
 #define NORMAL "0"
 #define BOLD "1"
+#define RED_FG "31"
 #define GREEN_FG "32"
+    TERM_RED_BOLD = "\e[" BOLD ";" RED_FG "m";
     TERM_GREEN_BOLD = "\e[" BOLD ";" GREEN_FG "m";
     TERM_NORMAL = "\e[" NORMAL "m";
 #undef BOLD
 #undef NORMAL
+#undef RED_FG
 #undef GREEN_FG
   }
 }
 
 void term_demo()
 {
+  printf("%s-- red bold --\n", TERM_RED_BOLD);
   printf("%s-- green bold --\n", TERM_GREEN_BOLD);
   printf("%s-- normal --\n", TERM_NORMAL);
 }
--- platform/io/terminal.h
+++ platform/io/terminal.h
@@ -1,6 +1,7 @@
 // Copyright (c) 2024 Robert Hildebrandt. All rights reserved.
 #pragma once
 
+extern const char* TERM_RED_BOLD;
 extern const char* TERM_GREEN_BOLD;
 extern const char* TERM_NORMAL;
 
