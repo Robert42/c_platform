5c058d57efd67cb78b7a7d3f474ef0d6f408a060
Robert <robert.3dgamedev@gmail.com>
2024-08-20 17:30:14 +0200

    Introduce function to quickly initialize scripts

---
+++ c_script.h
@@ -0,0 +1,6 @@
+// Copyright (c) 2024 Robert Hildebrandt. All rights reserved.
+#pragma once
+
+void c_script_init()
+{
+}
--- tdd.c
+++ tdd.c
@@ -1,7 +1,10 @@
 #!/usr/bin/env -S tcc -run
 // Copyright (c) 2024 Robert Hildebrandt. All rights reserved.
 
+#include "c_script.h"
+
 int main(int argc, const char** argv)
 {
+  c_script_init();
   return 0;
 }
