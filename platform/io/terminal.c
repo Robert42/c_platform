58e3630cf489b796f89d1bbd3d07075aa018a75e
Robert <robert.3dgamedev@gmail.com>
2024-08-20 20:01:11 +0200

    Add nonbold variants

--- platform/io/terminal.c
+++ platform/io/terminal.c
@@ -1,6 +1,8 @@
 // Copyright (c) 2024 Robert Hildebrandt. All rights reserved.
 #include "terminal.h"
 
+const char* TERM_RED = "";
+const char* TERM_GREEN = "";
 const char* TERM_RED_BOLD = "";
 const char* TERM_GREEN_BOLD = "";
 const char* TERM_NORMAL = "";
@@ -19,7 +21,9 @@ void term_init()
 #define BOLD "1"
 #define RED_FG "31"
 #define GREEN_FG "32"
+    TERM_RED = "\e[" NORMAL ";" RED_FG "m";
     TERM_RED_BOLD = "\e[" BOLD ";" RED_FG "m";
+    TERM_GREEN = "\e[" NORMAL ";" GREEN_FG "m";
     TERM_GREEN_BOLD = "\e[" BOLD ";" GREEN_FG "m";
     TERM_NORMAL = "\e[" NORMAL "m";
 #undef BOLD
@@ -31,7 +35,9 @@ void term_init()
 
 void term_demo()
 {
+  printf("%s-- red --\n", TERM_RED);
   printf("%s-- red bold --\n", TERM_RED_BOLD);
+  printf("%s-- green --\n", TERM_GREEN);
   printf("%s-- green bold --\n", TERM_GREEN_BOLD);
   printf("%s-- normal --\n", TERM_NORMAL);
 }
--- platform/io/terminal.h
+++ platform/io/terminal.h
@@ -1,6 +1,9 @@
 // Copyright (c) 2024 Robert Hildebrandt. All rights reserved.
 #pragma once
 
+extern const char* TERM_RED;
+extern const char* TERM_GREEN;
+
 extern const char* TERM_RED_BOLD;
 extern const char* TERM_GREEN_BOLD;
 extern const char* TERM_NORMAL;
