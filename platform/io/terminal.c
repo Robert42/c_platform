cd9d13e4700d54a8720a108ae3322ff3619abb55
Robert <robert.3dgamedev@gmail.com>
2024-08-20 20:03:46 +0200

    Add yellow

--- platform/io/terminal.c
+++ platform/io/terminal.c
@@ -3,8 +3,12 @@
 
 const char* TERM_RED = "";
 const char* TERM_GREEN = "";
+const char* TERM_YELLOW = "";
+
 const char* TERM_RED_BOLD = "";
 const char* TERM_GREEN_BOLD = "";
+const char* TERM_YELLOW_BOLD = "";
+
 const char* TERM_NORMAL = "";
 
 void term_init()
@@ -21,15 +25,19 @@ void term_init()
 #define BOLD "1"
 #define RED_FG "31"
 #define GREEN_FG "32"
+#define YELLOW_FG "33"
     TERM_RED = "\e[" NORMAL ";" RED_FG "m";
     TERM_RED_BOLD = "\e[" BOLD ";" RED_FG "m";
     TERM_GREEN = "\e[" NORMAL ";" GREEN_FG "m";
     TERM_GREEN_BOLD = "\e[" BOLD ";" GREEN_FG "m";
+    TERM_YELLOW = "\e[" NORMAL ";" YELLOW_FG "m";
+    TERM_YELLOW_BOLD = "\e[" BOLD ";" YELLOW_FG "m";
     TERM_NORMAL = "\e[" NORMAL "m";
 #undef BOLD
 #undef NORMAL
 #undef RED_FG
 #undef GREEN_FG
+#undef YELLOW_FG
   }
 }
 
@@ -39,5 +47,7 @@ void term_demo()
   printf("%s-- red bold --\n", TERM_RED_BOLD);
   printf("%s-- green normal --\n", TERM_GREEN);
   printf("%s-- green bold --\n", TERM_GREEN_BOLD);
+  printf("%s-- yellow normal --\n", TERM_YELLOW);
+  printf("%s-- yellow bold --\n", TERM_YELLOW_BOLD);
   printf("%s-- normal --\n", TERM_NORMAL);
 }
--- platform/io/terminal.h
+++ platform/io/terminal.h
@@ -3,9 +3,12 @@
 
 extern const char* TERM_RED;
 extern const char* TERM_GREEN;
+extern const char* TERM_YELLOW;
 
 extern const char* TERM_RED_BOLD;
 extern const char* TERM_GREEN_BOLD;
+extern const char* TERM_YELLOW_BOLD;
+
 extern const char* TERM_NORMAL;
 
 void term_init();
