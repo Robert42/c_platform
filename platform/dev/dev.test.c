9482b526bfcfa104e11e294334bd0c88e8e65834
Robert <robert.3dgamedev@gmail.com>
2024-08-22 08:53:13 +0200

    Fix test

--- platform/dev/dev.test.c
+++ platform/dev/dev.test.c
@@ -4,6 +4,8 @@
 
 void dev_test()
 {
+  int xs[2];
+
   EXPECT_ASSERT(
   EXPECT_ASSERT(
   );
@@ -17,17 +19,17 @@ void dev_test()
   EXPECT_ASSERT( assert_usize_lt(1, 1); );
   EXPECT_ASSERT( assert_usize_lt(1, 0); );
 
-  assert_ptr_eq(1, 1);
-  EXPECT_ASSERT( assert_ptr_eq(0, 1); );
-  EXPECT_ASSERT( assert_ptr_eq(1, 0); );
+  assert_ptr_eq(&xs[0], &xs[0]);
+  EXPECT_ASSERT( assert_ptr_eq(&xs[0], &xs[1]); );
+  EXPECT_ASSERT( assert_ptr_eq(&xs[1], &xs[0]); );
 
   debug_assert_usize_lt(0, 1);
   EXPECT_DEBUG_ASSERT( debug_assert_usize_lt(1, 1); );
   EXPECT_DEBUG_ASSERT( debug_assert_usize_lt(1, 0); );
 
-  debug_assert_ptr_lte(0, dev_test);
-  debug_assert_ptr_lte(0, 0);
-  EXPECT_DEBUG_ASSERT( debug_assert_ptr_lte(dev_test, 0); );
+  debug_assert_ptr_lte(&xs[0], &xs[1]);
+  debug_assert_ptr_lte(&xs[0], &xs[0]);
+  EXPECT_DEBUG_ASSERT( debug_assert_ptr_lte(&xs[1], &xs[0]); );
 
   EXPECT_DEBUG_ASSERT(
   EXPECT_DEBUG_ASSERT(
