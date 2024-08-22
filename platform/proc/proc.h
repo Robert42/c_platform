606073227a72a9ae8262f4658bbc96fb9f4d6f33
Robert <robert.3dgamedev@gmail.com>
2024-08-22 08:41:10 +0200

    Decided to implement only a blocking versoin for now

--- platform/proc/proc.h
+++ platform/proc/proc.h
@@ -1,21 +1,20 @@
 // Copyright (c) 2024 Robert Hildebrandt. All rights reserved.
 #pragma once
 
-struct Proc_Exec_Settings
+struct Proc_Exec_Blocking_Settings
 {
-  bool block : 1; // wait for the process to finish
   bool capture_stdout : 1;
   bool capture_stderr : 1;
   Mem_Region* region_stdout;
   Mem_Region* region_stderr;
 };
 
-struct Proc_Exec_Result
+struct Proc_Exec_Blocking_Result
 {
   int result;
   const char* captured_stdout;
   const char* captured_stderr;
 };
 
-struct Proc_Exec_Result proc_exec(const char* command, struct Proc_Exec_Settings settings);
+struct Proc_Exec_Blocking_Result proc_exec_blocking(const char* command, struct Proc_Exec_Blocking_Settings settings);
 
