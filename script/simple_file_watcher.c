2ae3e1eccaa6d25135856caaf6eace006a1ae57a
Robert <robert.3dgamedev@gmail.com>
2024-08-23 22:10:14 +0200

    Init inotify

--- script/simple_file_watcher.c
+++ script/simple_file_watcher.c
@@ -5,7 +5,15 @@ struct Simple_File_Watcher simple_file_watcher_init(const char* root, Fn_File_Fi
 {
   struct Simple_File_Watcher watcher = {
     .filter = filter,
+#ifdef __linux__
+    .watcher_fd = inotify_init(),
+#endif
   };
+
+#ifdef __linux__
+  LINUX_ASSERT_NE(watcher.watcher_fd, -1);
+  // TODO: create watchers for each directory
+#endif
 
   return watcher;
 }
