bb7b465d702b9634431674cdb4d9b66427000c99
Robert <robert.3dgamedev@gmail.com>
2024-08-25 18:29:58 +0200

    Add test after the second swap

--- utils/collections/set/setintcddo.test.c
+++ utils/collections/set/setintcddo.test.c
@@ -40,4 +40,43 @@ void setintcddo_test()
   debug_assert_int_eq(setintcddo_insert(&xs, 42), SETINTCDDOC_ALREADY_INSERTED);
   debug_assert_usize_eq(xs.len_new, 3);
   debug_assert_usize_eq(xs.len_old, 0);
+
+  setintcddo_reset(&xs);
+  debug_assert_usize_eq(xs.len_new, 0);
+  debug_assert_usize_eq(xs.len_old, 3);
+
+  debug_assert_int_eq(setintcddo_insert(&xs, 42), SETINTCDDOC_OLD);
+  debug_assert_int_eq(setintcddo_insert(&xs, 42), SETINTCDDOC_ALREADY_INSERTED);
+  debug_assert_usize_eq(xs.len_new, 1);
+  debug_assert_usize_eq(xs.len_old, 2);
+
+  debug_assert_int_eq(setintcddo_insert(&xs, 123), SETINTCDDOC_NEW);
+  debug_assert_int_eq(setintcddo_insert(&xs, 123), SETINTCDDOC_ALREADY_INSERTED);
+  debug_assert_int_eq(setintcddo_insert(&xs, 42), SETINTCDDOC_ALREADY_INSERTED);
+  debug_assert_usize_eq(xs.len_new, 2);
+  debug_assert_usize_eq(xs.len_old, 2);
+
+  debug_assert_int_eq(setintcddo_insert(&xs, 37), SETINTCDDOC_OLD);
+  debug_assert_int_eq(setintcddo_insert(&xs, 123), SETINTCDDOC_ALREADY_INSERTED);
+  debug_assert_int_eq(setintcddo_insert(&xs, 42), SETINTCDDOC_ALREADY_INSERTED);
+  debug_assert_int_eq(setintcddo_insert(&xs, 37), SETINTCDDOC_ALREADY_INSERTED);
+  debug_assert_usize_eq(xs.len_new, 3);
+  debug_assert_usize_eq(xs.len_old, 1);
+
+  debug_assert_int_eq(setintcddo_insert(&xs, 1337), SETINTCDDOC_NEW);
+  debug_assert_int_eq(setintcddo_insert(&xs, 123), SETINTCDDOC_ALREADY_INSERTED);
+  debug_assert_int_eq(setintcddo_insert(&xs, 42), SETINTCDDOC_ALREADY_INSERTED);
+  debug_assert_int_eq(setintcddo_insert(&xs, 37), SETINTCDDOC_ALREADY_INSERTED);
+  debug_assert_int_eq(setintcddo_insert(&xs, 1337), SETINTCDDOC_ALREADY_INSERTED);
+  debug_assert_usize_eq(xs.len_new, 4);
+  debug_assert_usize_eq(xs.len_old, 1);
+
+  debug_assert_int_eq(setintcddo_insert(&xs, 137), SETINTCDDOC_OLD);
+  debug_assert_int_eq(setintcddo_insert(&xs, 123), SETINTCDDOC_ALREADY_INSERTED);
+  debug_assert_int_eq(setintcddo_insert(&xs, 42), SETINTCDDOC_ALREADY_INSERTED);
+  debug_assert_int_eq(setintcddo_insert(&xs, 37), SETINTCDDOC_ALREADY_INSERTED);
+  debug_assert_int_eq(setintcddo_insert(&xs, 1337), SETINTCDDOC_ALREADY_INSERTED);
+  debug_assert_int_eq(setintcddo_insert(&xs, 137), SETINTCDDOC_ALREADY_INSERTED);
+  debug_assert_usize_eq(xs.len_new, 5);
+  debug_assert_usize_eq(xs.len_old, 0);
 }
