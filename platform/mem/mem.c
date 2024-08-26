1626252a68e078ea70da3af8fe3fb3621762f534
Robert <robert.3dgamedev@gmail.com>
2024-08-26 19:06:55 +0200

    Check that the scratch was valid before swapping

--- platform/mem/mem.c
+++ platform/mem/mem.c
@@ -6,11 +6,13 @@ void _swap_scratch(Mem_Region scratch_1, Mem_Region scratch_2)
 {
   if(SCRATCH.end == scratch_1.end)
   {
+    debug_assert_ptr_lte_lte(scratch_1.begin, SCRATCH.begin, scratch_1.end);
     SCRATCH = scratch_2;
   }
   else
   {
     debug_assert_ptr_eq(SCRATCH.end, scratch_2.end);
+    debug_assert_ptr_lte_lte(scratch_2.begin, SCRATCH.begin, scratch_2.end);
     SCRATCH = scratch_1;
   }
 }
