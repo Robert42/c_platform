75ff836ad763b5f927fe03afffd734864d424e94
Robert <robert.3dgamedev@gmail.com>
2024-08-29 08:43:07 +0200

    Fix warning

--- platform/time/timer.c
+++ platform/time/timer.c
@@ -7,28 +7,30 @@ const char* time_format_short_duration(u64 time, Mem_Region* region)
 {
   long double seconds = (long double)time / (long double)timer_freq;
 
-  const char* fmt = NULL;
+  const char* unit = NULL;
+  bool two_digits = false;
 
   if(seconds >= 1)
   {
-    fmt = "%.2f s";
+    unit = "s";
+    two_digits = true;
   }else if(seconds*1000 >= 1)
   {
     seconds *= 1000;
-    fmt = "%.0f ms";
+    unit = "ms";
   }else if(seconds*1000*1000 >= 1)
   {
     seconds *= 1000 * 1000;
-    fmt = "%.0f us";
+    unit = "us";
   }else
   {
     seconds *= 1000 * 1000 * 1000;
-    fmt = "%.0f ns";
+    unit = "ns";
   }
 
   const usize available = mem_region_available_bytes(region);
 
-  const int formatted_len = snprintf(region->begin, available, fmt, (double)seconds);
+  const int formatted_len = two_digits ? snprintf(region->begin, available, "%.2f %s", (double)seconds, unit) : snprintf(region->begin, available, "%.0f %s", (double)seconds, unit);
   assert_usize_lte_lt(0, formatted_len, available); // out of memory?
 
   char* text = region->begin;
