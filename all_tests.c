2a0368ce734ca1da249e85c8fe0ead18d5cef11e
Robert <robert.3dgamedev@gmail.com>
2024-09-03 19:09:27 +0200

    Working on static analysis, again

--- all_tests.c
+++ all_tests.c
@@ -1,11 +1,11 @@
 // Copyright (c) 2024 Robert Hildebrandt. All rights reserved.
 // Currently, there are no other tests outside of the unit tests
 // TODO: include unittest, again
-// #include "unit_test.c"
+#include "unit_test.c"
 
-#include "platform/prelude.h"
-#include "platform/mem/mem.c"
-#include "utils/fmt.c"
+// #include "platform/prelude.h"
+// #include "platform/mem/mem.c"
+// #include "utils/fmt.c"
 
 // #include "platform/mem/mem.test.c"
 // #include "utils/fmt.test.c"
