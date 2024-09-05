c34f391f294c2427c80951fbec1107b325a25d5d
Robert <robert.3dgamedev@gmail.com>
2024-09-05 17:09:02 +0200

    Refactor tests

--- utils/str.test.c
+++ utils/str.test.c
@@ -4,8 +4,11 @@
 
 void str_test()
 {
-  u8 BUFFER[4096];
-  Mem_Region region = MEM_REGION_FROM_ARRAY(BUFFER);
-  
-  assert_cstr_eq(cstr_fmt(&region, "Hello, %s!", "World"), "Hello, World!");
+  // cstr_fmt
+  {
+    u8 BUFFER[4096];
+    Mem_Region region = MEM_REGION_FROM_ARRAY(BUFFER);
+    
+    assert_cstr_eq(cstr_fmt(&region, "Hello, %s!", "World"), "Hello, World!");
+  }
 }
