3fa31beea1ad56d1d4f7a73839a232b846d7d7d6
Robert <robert.3dgamedev@gmail.com>
2024-09-06 22:28:18 +0200

    Fix sign comparison

--- platform/io/file.c
+++ platform/io/file.c
@@ -14,7 +14,7 @@ void file_text_create_from_cstr_if_different(const Path p, const char* str)
   const usize len = strlen(str);
 
   const ssize file_size = _file_size(p.cstr);
-  if(file_size>=0 && file_size == len)
+  if(file_size>=0 && (usize)file_size == len)
   {
     const Mem_Region prev_stack = STACK;
     Bytes_Mut bytes = _read_all_file_bytes(p.cstr, &STACK);
