d01a0cec6d7aa8794ef3cc4e2f073e27c6a7ee9a
Robert <robert.3dgamedev@gmail.com>
2024-09-06 15:00:51 +0200

    Bugfix: `file_text_create_from_cstr_if_different` works now even if the file does not exist

--- utils/file.c
+++ utils/file.c
@@ -12,6 +12,8 @@ void file_text_create_from_cstr_if_different(const Path p, const char* str)
 {
   const usize len = strlen(str);
 
+  const ssize file_size = _file_size(p.cstr);
+  if(file_size>=0 && file_size == len)
   {
     // TODO: introduce STACK region
     Bytes bytes = _read_all_file_bytes(p.cstr, &SCRATCH);
