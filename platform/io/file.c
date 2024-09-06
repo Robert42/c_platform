51534849ad86b7433eed73f9f199d6fb8db8d1df
Robert <robert.3dgamedev@gmail.com>
2024-09-06 21:56:51 +0200

    Refactor

--- platform/io/file.c
+++ platform/io/file.c
@@ -10,6 +10,7 @@ void file_text_create_from_cstr(Path p, const char* str)
 
 void file_text_create_from_cstr_if_different(const Path p, const char* str)
 {
+  bool already_exists_with_same_content = false;
   const usize len = strlen(str);
 
   const ssize file_size = _file_size(p.cstr);
@@ -19,9 +20,10 @@ void file_text_create_from_cstr_if_different(const Path p, const char* str)
     Bytes_Mut bytes = _read_all_file_bytes(p.cstr, &SCRATCH);
 
     if(bytes.end - bytes.begin == len && memcmp(bytes.begin, str, len)==0)
-      return;
+      already_exists_with_same_content = true;
   }
 
-  _create_file_from_bytes(p.cstr, str, len);
+  if(!already_exists_with_same_content)
+    _create_file_from_bytes(p.cstr, str, len);
 }
 
