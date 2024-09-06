1a84788f709713f13d431cb2887d71b96806d006
Robert <robert.3dgamedev@gmail.com>
2024-09-06 21:57:59 +0200

    Use stack region when comparing files

--- platform/io/file.c
+++ platform/io/file.c
@@ -16,11 +16,13 @@ void file_text_create_from_cstr_if_different(const Path p, const char* str)
   const ssize file_size = _file_size(p.cstr);
   if(file_size>=0 && file_size == len)
   {
-    // TODO: introduce STACK region
-    Bytes_Mut bytes = _read_all_file_bytes(p.cstr, &SCRATCH);
+    const Mem_Region prev_stack = STACK;
+    Bytes_Mut bytes = _read_all_file_bytes(p.cstr, &STACK);
 
     if(bytes.end - bytes.begin == len && memcmp(bytes.begin, str, len)==0)
       already_exists_with_same_content = true;
+
+    STACK = prev_stack;
   }
 
   if(!already_exists_with_same_content)
--- unit_test.c
+++ unit_test.c
@@ -15,13 +15,16 @@
 #endif
 
 static u8 _SCRATCH_BUFFER_1[1024*1024] = {0};
+static u8 _STACK_BUFFER[1024*1024] = {0};
 Mem_Region SCRATCH = {0};
+Mem_Region STACK = {0};
 
 int main(int argc, const char** argv)
 {
   platform_init();
 
   SCRATCH = MEM_REGION_FROM_ARRAY(_SCRATCH_BUFFER_1);
+  STACK = MEM_REGION_FROM_ARRAY(_STACK_BUFFER);
 
 #if 0
   term_demo();
