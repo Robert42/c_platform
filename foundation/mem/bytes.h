b9dbedf8910bbc0ba1f62329be62c7945d9f5f15
Robert <robert.3dgamedev@gmail.com>
2024-09-06 21:50:32 +0200

    Refactor

--- foundation/mem/bytes.h
+++ foundation/mem/bytes.h
@@ -1,9 +1,12 @@
 // Copyright (c) 2024 Robert Hildebrandt. All rights reserved.
 #pragma once
 
-typedef struct _platform_io_Bytes
+struct _platform_io_Bytes;
+typedef struct _platform_io_Bytes Bytes;
+
+struct _platform_io_Bytes
 {
   u8* begin;
   u8* end;
-} Bytes;
+};
 
