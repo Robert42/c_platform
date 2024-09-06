164adf0e96fbbb06c82608eac71e1b4feb467226
Robert <robert.3dgamedev@gmail.com>
2024-09-06 17:34:48 +0200

    Add header for primitive types

--- foundation/prelude.h
+++ foundation/prelude.h
@@ -2,3 +2,4 @@
 #pragma once
 
 #include "env.h"
+#include "ty/prim.h"
---
+++ foundation/ty/prim.h
@@ -0,0 +1,11 @@
+// Copyright (c) 2024 Robert Hildebrandt. All rights reserved.
+#pragma once
+
+#if ENV_COMPILER == COMPILER_TCC
+
+
+#else // ENV_COMPILER == COMPILER_TCC
+
+
+#endif
+
