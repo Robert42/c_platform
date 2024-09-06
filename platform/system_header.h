6447a9894c828a914032f484f5c79057c42a38d6
Robert <robert.3dgamedev@gmail.com>
2024-09-06 09:23:17 +0200

    Declare str together with teh integers

--- platform/system_header.h
+++ platform/system_header.h
@@ -91,6 +91,9 @@ typedef  int64_t s64;
 typedef size_t usize;
 typedef ssize_t ssize;
 
+struct _utils_str;
+typedef struct _utils_str str;
+
 #ifdef __linux__
 #include <time.h>
 #include <errno.h>
--- utils/str.h
+++ utils/str.h
@@ -1,9 +1,6 @@
 // Copyright (c) 2024 Robert Hildebrandt. All rights reserved.
 #pragma once
 
-struct _utils_str;
-typedef struct _utils_str str;
-
 struct _utils_str
 {
   const char* begin;
