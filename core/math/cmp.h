1204e320e23125ef23ba88e6485b68d2da58406a
Robert <robert.3dgamedev@gmail.com>
2024-09-07 12:43:44 +0200

    Marc the min_usize functions as temrianting

--- core/math/cmp.h
+++ core/math/cmp.h
@@ -2,9 +2,11 @@
 #pragma once
 
 /*@
+  terminates true;
   assigns \nothing;
   ensures \result <= x;
   ensures \result <= y;
   ensures \result==x || \result==y;
+  exits false;
 */
 usize min_usize(usize x, usize y);
