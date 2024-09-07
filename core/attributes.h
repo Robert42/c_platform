bc4a18905644d5a4aab3d2f16348666a15418750
Robert <robert.3dgamedev@gmail.com>
2024-09-07 14:50:33 +0200

    Use intrinsics

--- core/attributes.h
+++ core/attributes.h
@@ -6,5 +6,12 @@
 
 #define ATT_PRINTF(FORMAT_CSTR, FIRST_ARG_TO_CHECK) __attribute((format(printf, FORMAT_CSTR, FIRST_ARG_TO_CHECK)))
 
-// TODO use intrinsic
-#define LIKELY(X) X
+#if ENV_COMPILER == COMPILER_GCC || ENV_COMPILER == COMPILER_CLANG
+#define LIKELY(X) (__builtin_expect((X), 1))
+#define UNLIKELY(X) (__builtin_expect((X), 0))
+#else
+#define LIKELY(X) (X)
+#define UNLIKELY(X) (X)
+#endif
+
+
