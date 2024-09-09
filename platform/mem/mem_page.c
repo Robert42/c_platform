8bc26d771932c56ccef5eb37b9a1f9e0bc6a35b7
Robert <robert.3dgamedev@gmail.com>
2024-09-09 17:05:44 +0200

    Move `mem_page_init()` from platform specific code

--- platform/mem/mem_page.c
+++ platform/mem/mem_page.c
@@ -14,3 +14,14 @@ usize mem_page_ceil_multiple_usize(usize x)
 {
   return ceil_multiple_of_power_of_two_usize(x, MEM_PAGE_SIZE);
 }
+
+usize _mem_page_init_get_page_size(void);
+void mem_page_init()
+{
+  MEM_PAGE_SIZE = getpagesize();
+
+  // These two assertions together guarantee that no simd value will evver
+  // cross the boundary from a valid page to an invalid memory page.
+  debug_assert_usize_gte(MEM_PAGE_SIZE, 512/8);
+  debug_assert_bool_eq(is_power_of_two_or_zero_usize(MEM_PAGE_SIZE), true);
+}
--- platform/mem/mem_page.linux.c
+++ platform/mem/mem_page.linux.c
@@ -41,12 +41,7 @@ void mem_page_uncommit(u8* addr_begin, usize num_bytes)
   LINUX_ASSERT_EQ(addr, addr_begin);
 }
 
-void mem_page_init(void)
+usize _mem_page_init_get_page_size()
 {
-  MEM_PAGE_SIZE = getpagesize();
-
-  // These two assertions together guarantee that no simd value will evver
-  // cross the boundary from a valid page to an invalid memory page.
-  debug_assert_usize_gte(MEM_PAGE_SIZE, 512/8);
-  debug_assert_bool_eq(is_power_of_two_or_zero_usize(MEM_PAGE_SIZE), true);
+  return getpagesize();
 }
