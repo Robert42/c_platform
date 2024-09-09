f66e92305058d9328406b85faf18cb8411ffc353
Robert <robert.3dgamedev@gmail.com>
2024-09-09 14:46:54 +0200

    Add assertion

--- platform/mem/mem_page.linux.c
+++ platform/mem/mem_page.linux.c
@@ -5,4 +5,9 @@ usize MEM_PAGE_SIZE = 0;
 void mem_page_init(void)
 {
   MEM_PAGE_SIZE = getpagesize();
+
+  // These two assertions together guarantee that no simd value will evver
+  // cross the boundary from a valid page to an invalid memory page.
+  debug_assert_usize_gte(MEM_PAGE_SIZE, 512/8);
+  debug_assert_bool_eq(is_power_of_two_or_zero_usize(MEM_PAGE_SIZE), true);
 }
