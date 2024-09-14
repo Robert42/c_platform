03842bb6d0f95876008c0cab1a4a03d1032590e6
Robert <robert.3dgamedev@gmail.com>
2024-09-14 20:24:26 +0200

    Custom implementation of `time`

---
+++ utils/standalone/time.c
@@ -0,0 +1,24 @@
+// Copyright (c) 2024 Robert Hildebrandt. All rights reserved.
+
+// Custom implementation of the bash command `time`
+// ```
+// gcc -O2 ./utils/standalone/time.c -o ./utils/standalone/time
+// ```
+
+#include "../../c_script.h"
+
+int main(int argc, char** argv)
+{
+  c_script_init();
+
+  const u64 time_begin = timer_now();
+  struct Proc_Exec_Blocking_Result result = proc_exec_blocking(argv + 1, (struct Proc_Exec_Blocking_Settings){0});
+  const u64 time_end = timer_now();
+
+  const char* duration = time_format_short_duration(time_end-time_begin, &SCRATCH);
+
+  printf("\n%s==== time: %s%s%s ====%s\n", TERM.green, TERM.green_bold, duration, TERM.green, TERM.normal);
+
+  return result.exit_code;
+}
+
