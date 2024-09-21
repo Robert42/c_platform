d7d312f272fae3887c87836adf331862cf5ae159
Robert <robert.3dgamedev@gmail.com>
2024-09-21 18:49:08 +0200

    polish

--- utils/test.c
+++ utils/test.c
@@ -1,7 +1,7 @@
 // Copyright (c) 2024 Robert Hildebrandt. All rights reserved.
 #pragma once
 
-const char* const BANNER = "// ${BANNER}\n";
+const char* const BANNER = "// <BANNER>\n";
 
 #include "codegen/gen_table.c"
 #include "codegen/gen_table.test.c"
