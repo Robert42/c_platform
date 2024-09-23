a1b0b360ae5ed1be6bdaa09e9c6058334294320e
Robert <robert.3dgamedev@gmail.com>
2024-09-23 19:20:22 +0200

    Keep it simple: Remove soa codegen

--- utils/test.c
+++ utils/test.c
@@ -3,12 +3,9 @@
 
 const char* const BANNER = "// <BANNER>\n";
 
-#include "codegen/gen_table.c"
-#include "codegen/gen_table.test.c"
 #include "collections/set/setintcddo.test.c"
 
 void utils_test()
 {
-  gen_table_test();
   setintcddo_test();
 }
