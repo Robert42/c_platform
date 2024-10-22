cd78b8db43271c814e3fec93c4cee480ab8392d5
Robert <robert.hildebrandt@protonmail.com>
2024-10-22 17:34:47 +0200

    Compile the passed c file

--- utils/standalone/build_system.c
+++ utils/standalone/build_system.c
@@ -1,6 +1,13 @@
 // Copyright (c) 2024 Robert Hildebrandt. All rights reserved.
 
 #include "../../c_script.h"
+#include "libtcc.h"
+
+// Custom implementation of the bash command `time`
+// ```
+// cc -O2 ./utils/standalone/build_system.c `pkg-config --cflags --libs libtcc` -o ./utils/standalone/__build_c__
+// ```
+
 
 int main(int argc, char** argv)
 {
@@ -8,6 +15,17 @@ int main(int argc, char** argv)
 
   assert_int_gt(argc, 0);
   if(argc != 2)
+  {
     printf("USAGE: %s <FILE>\n", argv[0]);
+    return 0;
+  }
+
+  TCCState* tcc = tcc_new();
+
+  const Path include_path = path_join(path_parent(path_from_cstr(__FILE__)), path_from_cstr("include"));
+  tcc_add_include_path(tcc, include_path.cstr);
+  tcc_add_file(tcc, argv[1]);
+
+  tcc_delete(tcc);
 }
 
