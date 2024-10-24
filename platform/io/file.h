885a6be0de333d28288d5074b3275bcbc17406c6
Robert <robert.hildebrandt@protonmail.com>
2024-10-24 23:33:16 +0200

    Implement `file_text_read_to_cstr`

--- platform/io/file.h
+++ platform/io/file.h
@@ -3,7 +3,7 @@
 
 #include "path.h"
 
-const char* file_text_read_to_cstr(Path p, Mem_Region* region);
+char* file_text_read_to_cstr(Path p, Mem_Region* region);
 void file_text_create_from_cstr(Path p, const char* str);
 void file_text_create_from_cstr_if_different(Path p, const char* str);
 
