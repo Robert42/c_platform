499ebf2d2025552249d3f4f84d95070a2716b2b9
Robert <robert.hildebrandt@protonmail.com>
2024-10-24 23:33:36 +0200

    Read buildsystem ini

--- utils/standalone/build_system.c
+++ utils/standalone/build_system.c
@@ -1,7 +1,19 @@
 // Copyright (c) 2024 Robert Hildebrandt. All rights reserved.
 
 #include "../../c_script.h"
-#include "libtcc.h"
+
+struct Unit_Test
+{
+  const char* src;
+};
+
+struct Build_System_Data
+{
+  struct Unit_Test unit_test[1024];
+  usize unit_test_count;
+};
+
+static struct Ini_Format build_system_format(struct Build_System_Data* data);
 
 int main(int argc, char** argv)
 {
@@ -22,6 +34,26 @@ int main(int argc, char** argv)
     return 0;
   }
 
-  printf("TODO: parse <%s>\n", config_file.cstr);
+  struct Build_System_Data data = {};
+  struct Ini_Format ini_format = build_system_format(&data);
+  {
+    const Mem_Region _prev = STACK;
+    const char* text = file_text_read_to_cstr(config_file, &STACK);
+    ini_parse(&SCRATCH, &ini_format, text);
+    STACK = _prev;
+  }
+  printf("TODO:\n%s\n", ini_fmt(&SCRATCH, &ini_format));
 }
 
+static struct Ini_Format build_system_format(struct Build_System_Data* data)
+{
+  struct Ini_Format ini_format = {};
+
+  {
+    INI_FORMAT_SECTION_BEGIN(struct Unit_Test, unit_test, data->unit_test, ARRAY_LEN(data->unit_test), &data->unit_test_count);
+    INI_FORMAT_FIELD(src);
+    INI_FORMAT_SECTION_END();
+  }
+
+  return ini_format;
+}
