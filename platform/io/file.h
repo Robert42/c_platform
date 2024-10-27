37c56db5b9a9de835114ed2d9fb6383f02630a06
Robert <robert.hildebrandt@protonmail.com>
2024-10-27 09:15:49 +0100

    Add function `mkpath`

--- platform/io/file.h
+++ platform/io/file.h
@@ -7,6 +7,8 @@ char* file_text_read_to_cstr(Path p, Mem_Region* region);
 void file_text_create_from_cstr(Path p, const char* str);
 void file_text_create_from_cstr_if_different(Path p, const char* str);
 
+void mkpath(Path p);
+
 // low level
 void _create_file_from_bytes(const char* path, const void* bytes, usize num_bytes);
 Bytes_Mut _read_all_file_bytes(const char* path, Mem_Region* region);
