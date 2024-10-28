68225b4200cce71e49a4b24324e9369309e4707a
Robert <robert.hildebrandt@protonmail.com>
2024-10-28 09:59:46 +0100

    Provisoric implementation for skipping a dictionary

--- utils/parser/yaml.c
+++ utils/parser/yaml.c
@@ -2,15 +2,35 @@
 
 #include "yaml.h"
 
-struct Yaml_Node yaml_parse_doc_with_rest(Mem_Region* region, str* code)
+struct Yaml_Parse_Context
 {
-  struct Yaml_Node root = {
+  Mem_Region* region;
+};
+
+static struct Yaml_Node _yaml_parse_dict_block(struct Yaml_Parse_Context* ctx, const char** code)
+{
+  struct Yaml_Node dict = {
     .kind = YAML_DICT,
   };
-  return root;
+
+  while(**code != 0)
+  {
+    if(**code == '-')
+      dict.content.mapping_dict.len++;
+    *code += 1;
+  }
+  
+  return dict;
 }
 
-struct Yaml_Node yaml_parse_doc(Mem_Region* region, str code)
+struct Yaml_Node yaml_parse_doc_with_rest(Mem_Region* region, const char** code)
+{
+  struct Yaml_Parse_Context ctx;
+  ctx.region = region;
+  return _yaml_parse_dict_block(&ctx, code);
+}
+
+struct Yaml_Node yaml_parse_doc(Mem_Region* region, const char* code)
 {
   return yaml_parse_doc_with_rest(region, &code);
 }
--- utils/parser/yaml.test.c
+++ utils/parser/yaml.test.c
@@ -10,14 +10,28 @@ void yaml_test()
 
   // empty
   {
-    const str full = STR_LIT("");
-    str xs = full;
+    const char* full = "";
+    const char* xs = full;
     struct Yaml_Node root = yaml_parse_doc_with_rest(&STACK, &xs);
 
-    assert_ptr_eq(xs.begin, full.begin);
-    assert_ptr_eq(xs.end, full.end);
+    assert_ptr_eq(xs, full);
     assert(root.kind == YAML_DICT);
     assert(root.content.mapping_dict.len == 0);
+  }
+
+  // simple scalars
+  {
+    const char* full = 
+      "- name: John Doe\n"
+      "- hero: true\n"
+      "- age: 42\n"
+    ;
+    const char* xs = full;
+    struct Yaml_Node root = yaml_parse_doc_with_rest(&STACK, &xs);
+
+    assert_ptr_eq(xs, full+strlen(full));
+    assert(root.kind == YAML_DICT);
+    assert(root.content.mapping_dict.len == 3);
   }
 
   // ==== YAML documents ====
